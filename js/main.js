17d5c287eceb14c66cd035b910c2474f4ae3ec92
RafaStutz <rafaelstutz14@gmail.com>
2022-10-13 00:16:45 -0300

    fix: fix form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 const characterAmountRange = document.getElementById('characterAmountRange')
 const characterAmountNumber = document.getElementById('characterAmountNumber')
+const form = document.getElementById('passwordGeneratorForm')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
 
 const syncCharacterAmount = (e) => {
     const value = e.target.value
@@ -10,6 +14,9 @@ const syncCharacterAmount = (e) => {
 form.addEventListener('submit', e => {
     e.preventDefault()
     const characterAmount = characterAmountNumber.value
+    const includeUppercase = includeUppercaseElement.checked
+    const includeNumbers = includeNumbersElement.checked
+    const includeSymbols = includeSymbolsElement.checked
     const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
 })
 
